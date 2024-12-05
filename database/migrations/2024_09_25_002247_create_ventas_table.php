271388accadbf637f600eb423e5e4e603a08905f
AntonioRiosCovian <macmini@Mac-mini-de-Antonio-2.local>
2024-12-05 17:45:36 -0600

    tables fixed

--- database/migrations/2024_09_25_002247_create_ventas_table.php
+++ database/migrations/2024_09_25_002247_create_ventas_table.php
@@ -8,23 +8,22 @@ class CreateVentasTable extends Migration
 {
     public function up()
     {
-        Schema::table('ventas', function (Blueprint $table) {
-            $table->dropColumn('producto_id'); 
-            $table->dropColumn('cantidad');
-            $table->date('fecha')->default(now())->after('usuario_id');
-            $table->decimal('total', 8, 2)->after('fecha');
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id(); // bigint(20) UNSIGNED, primary key, auto-increment
+            $table->integer('cantidad')->unsigned(); // int(11), NOT NULL
+            $table->decimal('total', 10, 2); // decimal(10,2), NOT NULL
+            $table->unsignedBigInteger('usuario_id')->nullable(); // bigint(20) UNSIGNED, DEFAULT NULL
+            $table->timestamps(); // created_at and updated_at, NULL DEFAULT NULL
         });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
-        Schema::table('ventas', function (Blueprint $table) {
-            $table->foreignId('producto_id')->constrained('productos')->onDelete('cascade');
-            $table->integer('cantidad');
-            $table->decimal('precio_venta', 10, 2);
-            $table->dropColumn('fecha');
-            $table->dropColumn('total');
-        });
-    
+        Schema::dropIfExists('ventas');
     }
 }
